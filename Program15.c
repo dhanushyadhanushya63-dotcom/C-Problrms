5543fffd1ce4df6531b0ea6022415168b873537a
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-01-25 15:40:33 +0530

    Add files via upload
    Subtract two variables using c program

---
+++ Program15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+  int a=20;
+  int d=10;
+  int c=a-d;
+  printf("%d\n",c);
+  int b=40;
+  int e=10;
+  int res=b-e;
+  printf("%d",res);
+  
+   
+    return 0;
+}
