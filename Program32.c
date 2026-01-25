6584ab1401fafe4a163f9666f1d90d7c63b5ac76
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-01-25 15:53:58 +0530

    Add files via upload
    Print quotient and remainder of two integers

---
+++ Program32.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int d=10,g=5;
+    int quotient=d/g;
+    int remainder=d%g;
+    printf("Quotient:%d\n",quotient);
+    printf("Remainder:%d",remainder);
+    
+    return 0;
+}
