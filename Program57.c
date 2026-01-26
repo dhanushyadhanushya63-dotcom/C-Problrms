da2ce443f1c7d54c92be3d7040221c875d7dee21
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-01-26 10:49:41 +0530

    Add files via upload
    Find Greater of Two Numbers

---
+++ Program57.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1=56;
+    int num2=60;
+    if(num1>num2){
+        printf("Num1 is greater");
+    }else{
+        printf("Num2 is greater");
+    }
+    return 0;
+}
