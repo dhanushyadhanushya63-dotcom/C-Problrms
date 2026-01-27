d8a295953d38d29a21a5a3b61d0cee7d1fcac182
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-01-27 10:01:37 +0530

    Add files via upload
    Check 3-Digit Number

---
+++ Program63.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int num=333;
+    if(num>=100&&num<=999){
+        printf("It is a three digit number");
+    }else{
+        printf("Not a three digit number");
+    }
+    return 0;
+}
