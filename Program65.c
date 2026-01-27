4576cd98adeb20485635c39cb2eca453fe98b77d
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-01-27 10:11:24 +0530

    Add files via upload
    Grade Calculation

---
+++ Program65.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int mark=90;
+    if(mark>=90&&mark<=100){
+        printf("Grade A");
+    }
+    else if(mark>=75&&mark<=89){
+        printf("Grade B");
+    }else{
+        printf("Grade c");
+    }
+    return 0;
+}
