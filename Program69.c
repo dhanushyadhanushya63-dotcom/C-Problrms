5f34364a7ac7d1994f34401edaddc2a658ba42c5
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-02-07 10:19:24 +0530

    Add files via upload
    Type of Triangle

---
+++ Program69.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a=5;
+    int b=5;
+    int c=5;
+    if(a==b&&a==c&&b==c){
+        printf("Equal Triangle");
+    }else{
+        printf("Not equal triangle");
+    }
+    
+}
