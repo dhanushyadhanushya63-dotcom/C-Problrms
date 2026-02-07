3e8affb607e022865170f2793a1aba8dd5a6f097
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-02-07 10:15:27 +0530

    Add files via upload
    valid Triangle (Angles)

---
+++ Program68.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a=60;
+    int b=60;
+    int c=60;
+    if(a+b+c==180){
+        printf("Valid triangle");
+    }else{
+        printf("Invalid triangle");
+    }
+    
+}
