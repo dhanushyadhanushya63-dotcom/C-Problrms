6c02c6090f941a77cf881300e38e65a6915ae6e5
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-02-09 12:11:37 +0530

    Add files via upload
    Number Within Range

---
+++ Program70.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   int num=12;
+   if(num>=10 && num<=20){
+       printf("Number is within range");
+   }else{
+       printf("Number is not within range");
+   }
+
+    return 0;
+}
