60dd94b1fa21f2d314f89479c408d8fa00514971
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-02-09 12:51:56 +0530

    Add files via upload
    Smallest Using Ternary Operator

---
+++ Program73.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int num1=7;
+  int num2=90;
+  if(num1<num2){
+      printf("%d is the smallest",num1);
+  }else{
+      printf("%d is the smallest",num2);
+  }
+
+    return 0;
+}
