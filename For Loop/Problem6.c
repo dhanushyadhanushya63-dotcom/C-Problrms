9b34c6ce93908ee8169f1d9caa7fd66f10c7b585
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-02-20 18:11:11 +0530

    Add files via upload
    Factorial Calculator

---
+++ For Loop/Problem6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    int factorial=1;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        factorial=factorial*i;
+    }
+    printf("%d\n",factorial);
+    return 0;
+}
