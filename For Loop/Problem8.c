e4351d9d738d63577bdbc623006d49ac0225bef4
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-02-20 18:12:25 +0530

    Add files via upload
    Reverse a Number

---
+++ For Loop/Problem8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n;
+    int rev=0;
+    scanf("%d",&n);
+    for(;n!=0;n=n/10){
+      int rem=n%10;
+      rev=rev*10+rem;
+    }
+    printf("%d\n",rev);
+    return 0;
+}
