0619ead7c2e4946d5aa7f583ad1340b09f84089c
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-02-21 08:38:18 +0530

    Add files via upload

---
+++ Nested for Loops/Problem15.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int i;
+    int j;
+    char ch;
+    for(i=5;i>=1;i--){
+        ch='A';
+        for(j=1;j<=i;j++){
+            printf("%c ",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
