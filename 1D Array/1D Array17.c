4a2cde0690843a919febb78ff711b901eae68360
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-03-04 14:51:20 +0530

    Add files via upload
    Check Palindrome Sensor Data

---
+++ 1D Array/1D Array17.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+ int n;
+ scanf("%d",&n);
+ int arr[n];
+ for(int i=0;i<n;i++){
+     scanf("%d",&arr[i]);
+ }
+ int Palindrome=1;
+ for(int i=0;i<n/2;i++){
+     if(arr[i]!=arr[n-1-i]){
+         Palindrome=0;
+         break;
+     }
+ }
+ if(Palindrome==1){
+     printf("Yes");
+ }else{
+     printf("No");
+ }
+    return 0;
+}
