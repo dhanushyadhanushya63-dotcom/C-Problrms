4963e610ca2adae6c462ef8230b6be9df017c78d
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-03-07 20:30:33 +0530

    Add files via upload
    Maximum Difference Between Department Expenses

---
+++ 2D Array/2D Array6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    int diff,max=0;
+    int rowsum[r];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++){
+        int sum=0;
+        for(int j=0;j<c;j++){
+            sum=sum+arr[i][j];
+        }
+        rowsum[i]=sum;
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            diff=rowsum[i]-rowsum[j];
+            if(diff<0){
+                diff=-diff;
+            }
+            if(diff>max){
+                max=diff;
+            }
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
