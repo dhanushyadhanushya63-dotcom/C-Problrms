40e3499458898e7d594ded3788d6ee2e43c0758e
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-03-11 19:50:32 +0530

    Add files via upload

---
+++ String/str7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+  char str[100];
+  int count=0;
+  scanf("%s",str);
+  for(int i=0;str[i]!='\0';i++){
+      if((str[i]>='A'&&str[i]<='z')||(str[i]<='A'&&str[i]<='Z')){
+          if(str[i]!='A'&&str[i]!='E'&&str[i]!='I'&&str[i]!='O'&&str[i]!='U'&&str[i]!='a'&&str[i]!='e'&&str[i]!='i'&&str[i]!='o'&&str[i]!='u'){
+              count++;
+          }
+      }
+  }
+  printf("%d",count);
+    return 0;
+}
