dd0af8fafd8aa8b1f7e553b40cd65e09eb888049
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-03-11 19:54:16 +0530

    Add files via upload

---
+++ String/str10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ctype.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+    for(int i=0;str[i]!='\0';i++){
+        str[i]=tolower(str[i]);
+    }
+    printf("%s",str);
+    return 0;
+}
