8da0c3152c58ac172516c0bef5756089884503d2
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-03-13 10:39:08 +0530

    Add files via upload

---
+++ Function/program16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int fibonacci(int n){
+    int a=0;
+    int b=1;
+    int c;
+    for(int i=1;i<=n;i++){
+        printf("%d ",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    fibonacci(n);
+    return 0;
+}
