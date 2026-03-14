3db6052316e1369b4a90e09e0e65de5eb811ae86
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-03-14 10:23:48 +0530

    Add files via upload

---
+++ Function/program17.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int armstrong(int n){
+    int original=n;
+    int sum=0;
+    int r;
+    while(n!=0){
+        r=n%10;
+        sum=sum+(r*r*r);
+        n=n/10;
+    }
+    if(sum==original){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(armstrong(n)){
+        printf("Armstrong");
+    }
+    else{
+        printf("Not Armstrong");
+    }
+    return 0;
+}
