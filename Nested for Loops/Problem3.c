9c3864f61398ab0ab57fae84f45db17c2a441e43
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-03-23 18:10:38 +0530

    Update Problem3.c

--- Nested for Loops/Problem3.c
+++ Nested for Loops/Problem3.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main() {
     int i;
-    int j;
+    int j; 
     for(i=1;i<=5;i++){
         for(j=1;j<i+1;j++){
             printf("%d",j);
-        }
+        } 
         printf("\n");
     }
     return 0;
 }
+ 
